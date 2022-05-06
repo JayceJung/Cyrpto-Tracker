59e8f37438b2ad3851bded680dd3cd56c142b072
Jayce <jayce.jung@faire.com>
2022-05-06 16:57:26 -0400

    candlestick view

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -3,6 +3,8 @@ import { fetchCoinHist } from "../api";
 import ApexChart from "react-apexcharts";
 import FadeIn from "react-fade-in";
 import { ChartProps, IHistoricalData } from "../shared/Interface";
+import { useState } from "react";
+import styled from "styled-components";
 
 const Chart = ({ coinId }: ChartProps) => {
   const { isLoading, data } = useQuery<IHistoricalData[]>(
@@ -38,6 +40,9 @@ const Chart = ({ coinId }: ChartProps) => {
                 toolbar: {
                   show: false,
                 },
+                animations: {
+                  enabled: true,
+                },
               },
               grid: { show: false },
               stroke: {
@@ -46,7 +51,7 @@ const Chart = ({ coinId }: ChartProps) => {
               yaxis: {
                 decimalsInFloat: 2,
                 title: {
-                  text: "Price (USD)",
+                  text: "Closing Price (USD)",
                 },
               },
               xaxis: {
@@ -59,10 +64,51 @@ const Chart = ({ coinId }: ChartProps) => {
               colors: ["#0be881"],
             }}
           />
+          <ApexChart
+            type="candlestick"
+            series={[
+              {
+                name: "Price",
+                data: data?.map((price) => [
+                  Date.parse(price.time_close),
+                  [price.open, price.high, price.low, price.close],
+                ]) as unknown as number[],
+              },
+            ]}
+            options={{
+              theme: {
+                mode: "dark",
+              },
+              chart: {
+                height: 500,
+                width: 500,
+                background: "none",
+                toolbar: {
+                  show: false,
+                },
+                animations: {
+                  enabled: true,
+                },
+              },
+              grid: { show: false },
+              yaxis: {
+                decimalsInFloat: 2,
+                title: {
+                  text: "Price (USD)",
+                },
+              },
+              xaxis: {
+                type: "datetime",
+              },
+              fill: {
+                colors: undefined,
+              },
+              colors: ["#0be881"],
+            }}
+          />
         </FadeIn>
       )}
     </div>
   );
 };
-
 export default Chart;
